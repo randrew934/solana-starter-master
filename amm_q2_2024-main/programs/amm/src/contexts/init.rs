bfd1d2191863808bd7d0508b5fea98d8e9ed0296
Ral Andrew <randrew934@gmail.com>
2024-12-04 08:43:58 +0100

    Turbin3 AMM

---
+++ amm_q2_2024-main/programs/amm/src/contexts/init.rs
@@ -0,0 +1,78 @@
+use crate::error::AmmError;
+use crate::state::config::Config;
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+
+#[derive(Accounts)]
+#[instruction(seed:u64)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub mint_x: Box<InterfaceAccount<'info, Mint>>,
+    pub mint_y: Box<InterfaceAccount<'info, Mint>>,
+    // #[account(
+    //     init,
+    //     seeds = [b"lp", config.key.as_ref()],
+    //     bump,
+    //     payer = initializer,
+    //     mint::decimals = 6,
+    //     mint::authority = auth
+    // )]
+    // pub mint_lp: Box<InterfaceAccount<'info, Mint>>,
+    #[account(
+        init, 
+        payer = initializer,
+        associated_token::mint = mint_x,
+        associated_token::authority = auth
+    )]
+    pub vault_x: Box<InterfaceAccount<'info, TokenAccount>>,
+    #[account(
+        init, 
+        payer = initializer,
+        associated_token::mint = mint_y,
+        associated_token::authority = auth
+    )]
+    pub vault_y: Box<InterfaceAccount<'info, TokenAccount>>,
+    /// CHECK: this is safe
+    #[account(
+        seeds = [b"auth"],
+        bump
+    )]
+    pub auth: UncheckedAccount<'info>,
+    #[account(
+        init,
+        payer = initializer,
+        seeds = [b"config", seed.to_le_bytes().as_ref()],
+        bump,
+        space = Config::LEN
+    )]
+    pub config: Account<'info, Config>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn init(
+        &mut self,
+        bumps: &InitializeBumps,
+        seed: u64,
+        fee: u16,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        require!(fee <= 10000, AmmError::FeePercentErr);
+        self.config.init(
+            seed,
+            authority,
+            self.mint_x.key(),
+            self.mint_y.key(),
+            fee,
+            bumps.auth,
+            bumps.config,
+        );
+        Ok(())
+    }
+}
---
+++ amm_q2_2024-main/programs/amm/src/contexts/mod.rs
@@ -0,0 +1,14 @@
+pub mod init;
+pub use init::*;
+
+pub mod deposit;
+pub use deposit::*;
+
+pub mod update;
+pub use update::*;
+
+pub mod swap;
+pub use swap::*;
+
+pub mod withdraw;
+pub use withdraw::*;
